3f2aab08fbd771dfc4c2d8620f2b2e111d200692
Rishi Sheth <rishi.sheth0@gmail.com>
2019-08-16 15:04:40 -0600

    [SHOP-12928] migrated me some code

--- app.js
+++ app.js
@@ -1,102 +1,112 @@
-(function() {
-  return {
-    requests: {
-      fetchUsers: function(ids) {
-        return {
-          url: helpers.fmt('/api/v2/users/show_many.json?ids=%@&include=organizations,groups', ids.join(',')),
-          type: 'GET',
-          dataType: 'json'
-        };
+$(function () {
+
+  function getUriTemplatesFromSettings() {
+    return JSON.parse(this.settings.uri_templates);
+  }
+
+  _.memoize(function fieldsToWatch() {
+    return _.reduce(getUriTemplatesFromSettings(), function (memo, uri) {
+      var fields = _.map(uri.url.match(/\{\{(.+?)\}\}/g), function (f) { return f.slice(2, -2); });
+
+      return _.union(memo, fields);
+    }, []);
+  });
+
+  // helper function that creates our request
+  function fetchUsers(ids) {
+    return {
+      url: helpers.fmt('/api/v2/users/show_many.json?ids=%@&include=organizations,groups', ids.join(',')),
+      type: 'GET',
+      dataType: 'json'
+    };
+  }
+
+  function decorateUser(user) {
+    var name = (user.name || '').split(' ');
+
+    user.firstname = name[0] || '';
+    user.lastname = name[1] || '';
+
+    return user;
+  }
+
+  function findUserById(users, user_id) {
+    return _.find(users, function (user) {
+      return user.id == user_id;
+    }, this);
+  }
+
+  function getContext(data) {
+    var context = _.clone(this.containerContext());
+
+    if (context.ticket.requester.id) {
+      context.ticket.requester = decorateUser(findUserById(data.users, context.ticket.requester.id));
+
+      if (context.ticket.requester.organization_id) {
+        context.ticket.organization = _.find(data.organizations, function (org) {
+          return org.id == context.ticket.requester.organization_id;
+        });
       }
-    },
+    }
 
-    events: {
-      'app.created'             : 'onAppCreated',
-      '*.changed'               : function(e) {
-        if (_.contains(this.fieldsToWatch(), e.propertyName))
-          return this.onAppCreated();
-      },
-      'fetchUsers.done' : 'onFetchUsersDone'
-    },
+    if (context.ticket.assignee.user.id) {
+      context.ticket.assignee.user = decorateUser(findUserById(data.users, context.ticket.assignee.user.id));
+    }
 
-    onAppCreated: function() {
-      var userIds = _.compact(_.uniq([
-        (this.ticket().assignee().user() && this.ticket().assignee().user().id()),
-        this.currentUser().id(),
-        (this.ticket().requester() && this.ticket().requester().id())
-      ]));
+    //copying data not available on this.containerContext()
+    context.ticket.id = this.ticket().id();
+    context.ticket.description = this.ticket().description();
 
-      this.ajax('fetchUsers', userIds);
-    },
+    context.current_user = decorateUser(findUserById(data.users, this.currentUser().id()));
 
-    onFetchUsersDone: function(data) {
-      var templateUris = this.getUriTemplatesFromSettings(),
-          templateOptions = { interpolate : /\{\{(.+?)\}\}/g },
-          context = this.getContext(data),
-          uris = _.map(templateUris, function(uri){
-            try {
-              uri.url = _.template(uri.url, templateOptions)(context);
-              uri.title = _.template(uri.title, templateOptions)(context);
-            } catch(e) {
-              services.notify(e, 'error');
-            }
-            return uri;
-          }, this);
+    return context;
+  }
 
-      this.switchTo('list', { uris: uris });
-    },
-
-    getUriTemplatesFromSettings: function(){
-      return JSON.parse(this.settings.uri_templates);
-    },
-
-    getContext: function(data){
-      var context = _.clone(this.containerContext());
-
-      if (context.ticket.requester.id) {
-        context.ticket.requester = this.decorateUser(this.findUserById(data.users, context.ticket.requester.id));
-
-        if (context.ticket.requester.organization_id) {
-          context.ticket.organization = _.find(data.organizations, function(org) {
-            return org.id == context.ticket.requester.organization_id;
-          });
+  function onFetchUsersDone(data) {
+    var templateUris = getUriTemplatesFromSettings(),
+      templateOptions = { interpolate: /\{\{(.+?)\}\}/g },
+      context = getContext(data),
+      uris = _.map(templateUris, function (uri) {
+        try {
+          uri.url = _.template(uri.url, templateOptions)(context);
+          uri.title = _.template(uri.title, templateOptions)(context);
+        } catch (e) {
+          services.notify(e, 'error');
         }
-      }
-
-      if (context.ticket.assignee.user.id) {
-        context.ticket.assignee.user = this.decorateUser(this.findUserById(data.users, context.ticket.assignee.user.id));
-      }
-
-      //copying data not available on this.containerContext()
-      context.ticket.id = this.ticket().id();
-      context.ticket.description = this.ticket().description();
-
-      context.current_user = this.decorateUser(this.findUserById(data.users, this.currentUser().id()));
-
-      return context;
-    },
-
-    findUserById: function(users, user_id) {
-      return _.find(users, function(user) {
-        return user.id == user_id;
+        return uri;
       }, this);
-    },
 
-    decorateUser: function(user){
-      var name = (user.name || '').split(' ');
-
-      user.firstname = name[0] || '';
-      user.lastname = name[1] || '';
-
-      return user;
-    },
-
-    fieldsToWatch: _.memoize(function(){
-      return _.reduce(this.getUriTemplatesFromSettings(), function(memo, uri){
-        var fields = _.map(uri.url.match(/\{\{(.+?)\}\}/g), function(f){  return f.slice(2,-2); });
-
-        return _.union(memo, fields);
-      }, []);
-    })
+    this.switchTo('list', { uris: uris });
   };
-}());
+
+  function onAppCreated() {
+    var userIds = _.compact(_.uniq([
+      (this.ticket().assignee().user() && this.ticket().assignee().user().id()),
+      this.currentUser().id(),
+      (this.ticket().requester() && this.ticket().requester().id())
+    ]));
+
+    var settings = fetchUsers(userIds);
+
+    client.request(settings).then(function (data) {
+      onFetchUsersDone(data);
+    });
+  }
+
+  // Initialise the Zendesk JavaScript API client
+  // https://developer.zendesk.com/apps/docs/apps-v2
+  var client = ZAFClient.init();
+
+  client.on('app.registered', function () {
+    onAppCreated()
+  });
+
+  client.on('*.changed', function (e) {
+    if (_.contains(fieldsToWatch(), e.propertyName))
+      return onAppCreated();
+  });
+
+  client.on('fetchUsers.done', function () {
+    onFetchUsersDone()
+  });
+});
